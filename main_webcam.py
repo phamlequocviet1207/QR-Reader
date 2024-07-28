4a1387461b50964bafe27b8db5345bfdba889793
Viete <phamlequocviet1207@gmail.com>
2024-07-28 19:19:13 -0400

    fixing bugs

--- main_webcam.py
+++ main_webcam.py
@@ -1,11 +1,13 @@
 import datetime
-
 from pyzbar.pyzbar import decode
 import cv2
 import matplotlib.pyplot as plt
 import numpy as np
 import os
-import datetime
+from datetime import datetime
+import time
+import math
+
 cap = cv2.VideoCapture(0)
 
 authorized_users = []
@@ -18,6 +20,9 @@ with open ('./authorized_users.txt', 'r') as file:
 
 log_path = "./log.txt"
 
+most_recent_access = {}
+
+time_between_log = 10
 
 while True:
     ret, frame = cap.read()
@@ -31,11 +36,19 @@ while True:
         polygon = qr.polygon
 
         if data.decode() in authorized_users:
+            now = datetime.now()
             cv2.putText(frame, "ACCESS GRANTED", (rect.left, rect.top - 15), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,255,0), 2)
-            with open(log_path, 'a') as file:
-                now = datetime.time.now()
-                file.write('{}{}\n'.format(data.decode(), now))
-                file.close()
+            # print(time.time())
+            # print("now",now)
+            if data.decode() not in most_recent_access.keys() \
+                    or time.time() - most_recent_access[data.decode()] > time_between_log:
+
+                most_recent_access[data.decode()] = time.time()
+
+                with open(log_path, 'a') as file:
+
+                    file.write('{}, {}\n'.format(data.decode(), now))
+                    file.close()
         else:
             cv2.putText(frame, "ACCESS DENIED", (rect.left, rect.top - 15), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255),
                         2)
